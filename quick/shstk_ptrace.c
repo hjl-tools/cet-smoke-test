0218c5b432346b99db8ef5ae52df4136b29b76c8
Yu-cheng Yu <yu-cheng.yu@intel.com>
2018-09-06 15:04:16 -0700

    Add PTRACE quick test.
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>

---
+++ quick/shstk_ptrace.c
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Quick tests to verify PTRACE_POKEDATA can write to a shadow stack */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define TEST_PATTERN 0xa5a5a5a5a5a5a5a5UL
+
+void *get_last_shstk_page(void)
+{
+	void *shstk_page;
+
+	asm volatile("rdsspq %0\n": "=r" (shstk_page));
+	shstk_page = (void *)((unsigned long)shstk_page &
+			0xfffffffffffff000);
+	return shstk_page;
+}
+
+int main(int argc, char *argv[])
+{
+	int r, status;
+	pid_t child;
+	void *shstk_page;
+	unsigned long data;
+
+	child = fork();
+	if (child == 0) {
+		sleep(1);
+		shstk_page = get_last_shstk_page();
+//		printf("child: shstk page = %p\n", shstk_page);
+//		printf("child: *shstk_page=%016lx\n",
+//		       *(unsigned long *)shstk_page);
+		if (*(unsigned long *)shstk_page == TEST_PATTERN)
+			printf("OK\n");
+		else
+			printf("FAIL\n");
+		exit(0);
+	}
+
+	if (child < 0) {
+		printf("fork failed!\n");
+		return -1;
+	}
+
+	shstk_page = get_last_shstk_page();
+//	printf("self: shstk page = %p\n", shstk_page);
+//	printf("self: *shstk_page=%016lx\n",
+//	       *(unsigned long *)shstk_page);
+
+	r = ptrace(PTRACE_ATTACH, child, NULL, NULL);
+	if (r) {
+		printf("PTRACE_ATTACH failed!\n");
+		return -1;
+	}
+
+	r = waitpid(child, &status, 0);
+	if (r != child) {
+		printf("waitpid failed!\n");
+		return -1;
+	}
+
+	data = TEST_PATTERN;
+	r = ptrace(PTRACE_POKEDATA, child, shstk_page, data);
+	if (r) {
+		printf("PTRACE_POKEDATA failed!\n");
+		return -1;
+	}
+
+	ptrace(PTRACE_CONT, child, NULL, NULL);
+	return 0;
+}
