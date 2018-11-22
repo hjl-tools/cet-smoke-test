d3cc7caa39fce2e75ec708aa3a138a99ad7f62f9
H.J. Lu <hjl.tools@gmail.com>
2018-11-21 18:35:08 -0800

    quick/ibt_bitmap.c: Update ARCH_CET_LEGACY_BITMAP test
    The current CET kernel:
    
    https://github.com/yyu168/linux_cet
    
    changed legacy region bitmap allocation from kernel to user space and
    renumbered the prctl number from 0x3005 to 0x3006.  This patch updates
    glibc with:
    
    /* Enable legacy region bitmap with unsigned long long *addr:
         address: addr[0].
         size: addr[1].
     */
     # define ARCH_CET_LEGACY_BITMAP	0x3006

--- quick/ibt_bitmap.c
+++ quick/ibt_bitmap.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/mman.h>
 #include <asm/prctl.h>
 #include <sys/prctl.h>
 #include <x86intrin.h>
 
 int arch_prctl(int code, unsigned long *addr);
 
-#define ARCH_CET_LEGACY_BITMAP 0x3005
+#define ARCH_CET_LEGACY_BITMAP 0x3006
 
 void ibt_violation(void)
 {
@@ -28,26 +31,34 @@ void set_bitmap(unsigned char *bitmap, void *addr)
 	bitmap[byte] |= (0x01 << bit);
 }
 
+extern void *__libc_stack_end;
+
 int main(int argc, char *argv[])
 {
 	int r;
-	unsigned long args[2];
+	unsigned long long args[2];
 	unsigned char *bitmap;
 	unsigned long byte;
 	unsigned long bit;
+	size_t bitmap_size;
 
+	bitmap_size = ((uintptr_t) __libc_stack_end
+		       / sysconf(_SC_PAGESIZE) / 8);
+	bitmap = mmap (NULL, bitmap_size, PROT_READ | PROT_WRITE,
+		       MAP_ANON | MAP_PRIVATE | MAP_NORESERVE, -1, 0);
+	if (bitmap == MAP_FAILED) {
+		printf("mmap legacy bitmap failed.\n");
+		return -1;
+	}
+
+	args[0] = (uintptr_t) bitmap;
+	args[1] = bitmap_size;
 	r = arch_prctl(ARCH_CET_LEGACY_BITMAP, args);
 	if (r) {
 		printf("ARCH_CET_LEGACY_BITMAP failed.\n");
 		return -1;
 	}
 
-	if (args[0] == 0) {
-		printf("Bitmap.is NULL.\n");
-		return -1;
-	}
-
-	bitmap = (unsigned char *)args[0];
 	set_bitmap(bitmap, ibt_violation);
 	ibt_violation();
 	printf("OK\n");
